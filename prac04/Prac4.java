c0a0ccee4d20a06ebe1a8b8146cdfae346ae88c7
Rudra Patel <patelrudrahn676@gmail.com>
2025-05-06 14:29:05 +0530

    Minor fix

--- prac04/Prac4.java
+++ prac04/Prac4.java
@@ -2,14 +2,14 @@ class Prac4 {
     public void display(){
         int n=1;
         while (n<=100)
-            System.err.print(n++ + "\t");
+            System.out.print(n++ + "\t");
     }    
 
     public void fibonacci(){
         int num = 0;
         int next = 1;
         while (num<100) {
-            System.err.print(num+"\t");
+            System.out.print(num+"\t");
             int sum=num+next;
             num=next;
             next=sum;
@@ -18,10 +18,10 @@ class Prac4 {
 
     public static void main(String[] args) {
         Prac4 ps = new Prac4();
-        System.err.println("Printing 1-100:");
+        System.out.println("Printing 1-100:");
         ps.display();
-        System.err.println("\n");
-        System.err.println("Fibonacci Sequence till 100:");
+        System.out.println("\n");
+        System.out.println("Fibonacci Sequence till 100:");
         ps.fibonacci();
     }
 }
